1e3a27a81a8a097cf5fbd11516ff85e1dce1f6e5
MOGRAND <mograndpmg@gmail.com>
2024-01-27 18:21:15 +0300

    Added drawable resources and HomeScreen

--- app/src/main/java/com/example/bikestat/Navigation/NavGraph.kt
+++ app/src/main/java/com/example/bikestat/Navigation/NavGraph.kt
@@ -7,9 +7,6 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.bikestattest.HistoryScreen.HistoryScreenView
-import com.example.bikestattest.HomeScreen.HomeScreenView
-import com.example.bikestattest.StatisticsScreen.StatisticsScreenView
 
 
 @Composable
